0a9f92b51631fd2855f850c4bf3338516cded03a
Tanay Lodh <tanaylodh@Tanays-MacBook-Air.local>
2023-12-09 12:53:39 +0530

    inital setup

---
+++ src/server/db/index.ts
@@ -0,0 +1,11 @@
+import { env } from "@/env";
+
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
+
+const client = createClient({
+  url: env.DATABASE_URL,
+  authToken: env.DATABASE_AUTH_TOKEN,
+});
+
+export const db = drizzle(client);
