895c0323e1b117df3c86807609801e0e0965d6a7
tonoyondaweb <iamtanaylodh@gmail.com>
2023-12-13 09:15:22 +0530

    added new sign out ui flow

--- src/app/_components/auth-buttons/sign-out.tsx
+++ src/app/_components/auth-buttons/sign-out.tsx
@@ -2,40 +2,20 @@
 
 import React from "react";
 
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  useDisclosure,
-} from "@nextui-org/react";
 import { signOut } from "next-auth/react";
-export default function SignOutButton() {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+import { useRouter } from "next/navigation";
+
+import { Button } from "@nextui-org/react";
+
+export default function SignOutButtons() {
+  const router = useRouter();
 
   return (
-    <>
-      <Button onPress={onOpen}>Sign out</Button>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader>Signing out?</ModalHeader>
-              <ModalBody>
-                <p>Are you sure you want to sign out of commune?</p>
-              </ModalBody>
-              <ModalFooter>
-                <Button onPress={onClose}>No</Button>
-                <Button color="danger" onClick={() => signOut()}>
-                  Sign out
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
+    <div className="flex justify-end gap-x-2">
+      <Button onClick={() => router.back()}>No</Button>
+      <Button color="danger" onClick={() => signOut()}>
+        Sign out
+      </Button>
+    </div>
   );
 }
--- src/app/auth/signOut/page.tsx
+++ src/app/auth/signOut/page.tsx
@@ -1,5 +1,6 @@
-import SignOutButton from "@/app/_components/auth-buttons/sign-out";
+import SignOutButtons from "@/app/_components/auth-buttons/sign-out";
 import { getServerAuthSession } from "@/server/auth";
+import { Divider } from "@nextui-org/react";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -9,7 +10,13 @@ export default async function SignOutPage() {
   if (!session) redirect("/");
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <SignOutButton />
+      <div className="flex flex-col gap-y-5">
+        <p className="text-2xl font-semibold">Goodbye? 🙁</p>
+        <div className="flex flex-col gap-y-5 rounded-xl border border-default p-5">
+          <p>Are you sure you want to sign out of commune?</p>
+          <SignOutButtons />
+        </div>
+      </div>
     </main>
   );
 }
